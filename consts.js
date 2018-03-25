5a1c89f4b579dd76678b9ab55ed0bc8821d56f1b
Dan <dan@dan.pc>
2018-03-25 23:55:27 +0200

    ADD del to help

--- consts.js
+++ consts.js
@@ -6,4 +6,5 @@ export const keyboard = [
 export const helpText = `You can use this commands:
   /help to see this info again \u{2754}
   /list to see your tasks list \u{2714}
-  /add to add a new task \u{2795}`
+  /add [task] to add a new task \u{2795}
+  /del [task number] to remove task \u{274c}`
